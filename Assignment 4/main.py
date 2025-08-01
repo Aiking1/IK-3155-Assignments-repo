920b0bd7f558b6fbf776902065401135b11c849a
Aiking1 <isaac03king@gmail.com>
2025-08-01 15:52:33 -0400

    final edits with documentation

--- Assignment 4/main.py
+++ Assignment 4/main.py
@@ -14,10 +14,12 @@ class Item(BaseModel):
 def read_root():
         return {"Hello": "World"}
 
+#reading the item
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
         return {"item_id": item_id, "q": q}
 
+#updating the items
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
         return {"item_name": item.name, "item_id": item_id}
